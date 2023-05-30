a983786bfb0765ac2914ca8c69f3f09c36a5779d
allangbr <allanalmeidagbr@gmail.com>
2023-05-30 10:01:01 -0300

    criando a estrutura do projeto

--- locallibrary/locallibrary/urls.py
+++ locallibrary/locallibrary/urls.py
@@ -1,4 +1,4 @@
-"""mysite URL Configuration
+"""locallibrary URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path("polls/", include('polls.urls')),
     path('admin/', admin.site.urls),
-]
+    path('catalog/', include('catalog.urls')),
+    path('', RedirectView.as_view(url='/catalog/')),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- mysite/polls/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-
-from polls.models import Author, Presentation
-
-admin.site.register(Author)
-admin.site.register(Presentation)
